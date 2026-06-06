fb665ce1de286ea02e10586a1bff78a7fdf22d44
kedenise <ke562595@ucf.edu>
2026-06-05 21:17:24 -0400

    Update UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,4 +1,54 @@
 <?php
 
+	$inData = getRequestInfo();
+
+	$contactId = $inData["contactId"];
+	$userId = $inData["userId"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCIS4004", "CIS4004"); 	
+
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE ID=? AND UserID=?");
+		$stmt->bind_param("ssssii", $firstName, $lastName, $phone, $email, $contactId, $userId);
+		$stmt->execute();
+
+		if( $stmt->affected_rows > 0 )
+		{
+			returnWithError("");
+		}
+		else
+		{
+			returnWithError("No contact was updated");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
 
 ?>
