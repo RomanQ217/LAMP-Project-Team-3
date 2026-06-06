40cd75e8847f5496bf0df00f64e48d594139867e
kedenise <ke562595@ucf.edu>
2026-06-05 21:16:24 -0400

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,4 +1,48 @@
 <?php
+	$inData = getRequestInfo();
 
+	$contactId = $inData["contactId"];
+	$userId = $inData["userId"];
 
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCIS4004", "CIS4004");
+
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE ID=? AND UserID=?");
+		$stmt->bind_param("ii", $contactId, $userId);
+		$stmt->execute();
+
+		if ($stmt->affected_rows > 0)
+		{
+			returnWithError("");
+		}
+		else
+		{
+			returnWithError("No contact was deleted");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
 ?>
