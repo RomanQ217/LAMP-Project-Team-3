70bf45f47e54c962f707aa9be33713e2f7f1b989
kedenise <ke562595@ucf.edu>
2026-06-05 21:14:17 -0400

    Update AddContact.php

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,4 +1,46 @@
 <?php
 
+	$inData = getRequestInfo();
+
+	$userId = $inData["userId"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCIS4004", "CIS4004");
+
+	if( $conn->connect_error )
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT INTO Contacts (UserID, FirstName, LastName, Phone, Email) VALUES (?,?,?,?,?)");
+		$stmt->bind_param("issss", $userId, $firstName, $lastName, $phone, $email);
+		$stmt->execute();
+
+		$stmt->close();
+		$conn->close();
+
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
 
 ?>
